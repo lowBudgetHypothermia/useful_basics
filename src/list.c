5d18e88df9c4c099e5c6dcf54b3c96621fc277e8
s.hasert <s.hasert@gsi.de>
2025-11-17 16:54:35 +0100

    list: init

---
+++ src/list.c
@@ -0,0 +1,37 @@
+#include "list.h"
+
+int lst_push(struct list_t *lst, void *node) {
+    int rc = 0;
+    lst_node_t *new = malloc(sizeof(lst_node_t));
+    if (!new) {
+        rc = -1;
+        return rc;
+    }
+
+    new->data = node;
+
+    // TODO
+    // check if tail
+    new->prev = lst->tail;
+    lst->tail = new;
+
+    return rc;
+};
+
+int lst_pop(struct list_t *lst) {
+    int rc = 0;
+
+    // TODO
+    // check if tail
+    lst_node_t *tail = lst->tail;
+
+    // TODO
+    // check if prev
+    lst->tail = tail->prev;
+
+    // TODO
+    // check if free successful
+    free(tail);
+
+    return rc;
+};
